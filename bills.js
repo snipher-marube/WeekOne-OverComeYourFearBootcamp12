495fb289d27cdeefa762280722c36cdd4b13aaa5
MollyOuma <101042597+MollyOuma@users.noreply.github.com>
2022-09-26 20:00:44 +0300

    bill calculator

--- bills.js
+++ bills.js
@@ -1,4 +1,64 @@
 
+// //Access the form input values and then sum them..
+    
+        
+
+// // the submit button containing the net salary from the user..
+// //get the balance 
+// function submitFunction()
+// {
+
+//     let rent, shopping, electricity, water, transport, otherBills;
+          
+//         rent = parseInt(document.getElementById('rent').value);
+//         shopping = parseInt(document.getElementById('shopping').value);
+//         electricity = parseInt(document.getElementById('electricity-bill').value);
+//         water = parseInt(document.getElementById('water-bill').value);
+//         transport = parseInt(document.getElementById('transport').value);
+//         otherBills = parseInt(document.getElementById('bill-amount').value);
+//         const sum = rent + shopping + electricity + water + transport + otherBills;
+//         document.getElementById('total').value = sum;
+
+//     let netSalary;
+//     netSalary = parseInt(document.getElementById('salary').value);
+//     const balance =((document.getElementById('salary').value) - (document.getElementById('total').value));
+//     document.getElementById('balance').value = balance;
+// }
+// //should indicate warning when  less than 20% of the net salary...
+// if((document.getElementById('balance').value) < (20/100 * (document.getElementById('salary'))))
+// {
+//     document.getElementById('total').display = red;
+// }
+// else
+// {
+//     console.log('Below 20%');
+// }
+
+// //function to add categories to the form
+// function listFunction()
+// {
+//         let count = 0;
+//         if(count < 50)
+//         {
+//         let labelInput = document.createElement("input");
+//         labelInput.setAttribute("type", "text" );
+//         labelInput.setAttribute("name", "Bills");
+//         labelInput.setAttribute("placeholder", "bills");
+
+//         let amountInput= document.createElement("input");
+//         amountInput.setAttribute("type", "number");
+//         amountInput.setAttribute("name", "amount");
+//         amountInput.setAttribute("placeholder", "amount");
+//         amountInput.setAttribute("id", "other")
+
+//        document.getElementById('form-bills').append(labelInput , amountInput);
+       
+//        //document.getElementById('form-bills').append(inputs);
+//         }
+        
+//     }
+    
+ 
 //Access the form input values and then sum them..
 // global variable, used by listfunction
 var count = 0;
@@ -40,7 +100,7 @@ function submitFunction()
     water = parseInt(document.getElementById('water-bill').value);
     transport = parseInt(document.getElementById('transport').value);
     // check if otherbills exists, if not otherbills should be zero
-    var el = document.getElementById("0");
+    let el=document.getElementById("0");
     if(el == null){
         otherBills = 0;
     }
@@ -94,7 +154,7 @@ function listFunction()
         amountInput.setAttribute("type", "number");
         amountInput.setAttribute("name", "amount");
         amountInput.setAttribute("placeholder", "amount");
-        amountInput.setAttribute("id", count)
+        amountInput.setAttribute("id", "count")
         
         let Linebreak = document.createElement("br");
         document.getElementById('bill-add').append(labelInput , amountInput);
